2bcc6b635b588bfb5cff02772c462507e9e08e12
Kais Wass <kwassilima@gmail.com>
2023-06-23 16:14:48 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define ExitSuccess 0
+
+
+int main(){
+
+    std::string text = "Hello Mingw!";
+
+    std::cout << text;
+    system("pause");
+    return ExitSuccess;
+
+}//end main
